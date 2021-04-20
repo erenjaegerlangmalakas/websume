0cd1b5c075f0f62bca805b12e317c237f438aa7a
erenjaegerlangmalakas <trexcyfe.torrefiel@lsu.edu.ph>
2021-04-20 15:03:22 +0700

    added my project

---
+++ src/views/pages/Home.vue
@@ -0,0 +1,23 @@
+<template>
+  <ion-page>
+    <ion-header :translucent="true">
+    </ion-header>
+  </ion-page>
+</template>
+
+<script lang="ts">
+import {IonHeader, IonPage} from '@ionic/vue';
+import { defineComponent } from 'vue';
+
+export default defineComponent({
+  name: 'Home',
+  components: {
+    IonHeader,
+    IonPage
+  }
+});
+</script>
+
+<style scoped>
+
+</style>
